c2d1d8a66730f521bd4c5cc572ae1702b4864775
Petter Hersinger <petterhersinger@gmail.com>
2023-09-06 13:20:11 +0200

    TeamName shows TeamId and remove button does not work

--- Lab1/Controllers/PlayerController.cs
+++ Lab1/Controllers/PlayerController.cs
@@ -9,6 +9,7 @@ namespace Lab1.Controllers
         public static int playerCounter = 1;
         public static List<Player> players = new List<Player>();
         public static List<Team> teams = GetTeams();
+        public static List<PlayerTeamViewModel> playerTeams = new List<PlayerTeamViewModel>();
 
         private static List<Team> GetTeams()
         {
@@ -21,27 +22,10 @@ namespace Lab1.Controllers
             return teams;
         }
 
-        private Team GetTeamById(int teamId)
-        {
-            return teams.FirstOrDefault(team => team.TeamId == teamId);
-        }
-
         public IActionResult ShowPlayers()
         {
-            var playerTeamViewModelList = new List<PlayerTeamViewModel>();
 
-            foreach (var player in players)
-            {
-                var team = GetTeamById(player.TeamId);
-                var playerTeamViewModel = new PlayerTeamViewModel
-                {
-                    Player = player,
-                    Team = team
-                };
-                playerTeamViewModelList.Add(playerTeamViewModel);
-            }
-
-            return View(playerTeamViewModelList);
+            return View(playerTeams);
         }
 
         public IActionResult Create()
@@ -57,17 +41,22 @@ namespace Lab1.Controllers
         [HttpPost]
         public IActionResult Create(PlayerTeamViewModel playerTeamViewModel)
         {
-            if (ModelState.IsValid)
-            {
-                var player = playerTeamViewModel.Player;
-                player.Id = playerCounter++;
-                players.Add(player);
-                return RedirectToAction("ShowPlayers");
-            }
 
             var teams = GetTeams();
-            ViewBag.Teams = new SelectList(teams, "TeamId", "TeamName");
-            return View(playerTeamViewModel);
+            ViewData["Teams"] = new SelectList(teams, "TeamId", "TeamName");
+            var player = playerTeamViewModel.Player;
+            player.Id = playerCounter++;
+
+
+            if (!ModelState.IsValid)
+            {
+                return View(playerTeamViewModel);
+            }
+
+            playerTeams.Add(playerTeamViewModel);
+
+            return RedirectToAction("ShowPlayers");
+
         }
 
         [HttpPost]
